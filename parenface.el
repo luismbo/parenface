cd1ff4c92dedc9c5923cdbce739606367e930f2f
Luís Oliveira <loliveira@common-lisp.net>
2014-11-29 22:39:25 +0000

    Add missing license

--- parenface.el
+++ parenface.el
@@ -4,6 +4,21 @@
 ;; Version: 2.0
 ;; Maintainer: Luís Oliveira <loliveira@common-lisp.net>
 
+;; This file is not part of GNU Emacs.
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; The intended purpose of this package is to make parentheses less
